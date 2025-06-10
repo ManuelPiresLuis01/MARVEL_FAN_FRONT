a3ffa522e8119079350721d69003fd5854f97f8b
manuelpiresluis <manuel.pires@mamboo.co.ao>
2025-06-10 16:37:01 +0100

    feat: add dotenv configuration and .env files for environment variables

--- marvel_fan/vite.config.ts
+++ marvel_fan/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
-// https://vite.dev/config/
+dotenv.config()
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: parseInt(process.env.VITE_FRONT_PORT || '3000'), 
+  }
 })
